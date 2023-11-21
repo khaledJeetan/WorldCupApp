ef010324145acd3798a5ae4fbecba563a297e1f6
Khaled Jeetan <khalidgeetan@gmail.com>
2023-11-21 07:42:34 +0200

    Initial Commit

---
+++ WorldCupApp/WorldCupAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldCupAppApp.swift
+//  WorldCupApp
+//
+//  Created by Prince Jeetan on 21/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WorldCupAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
